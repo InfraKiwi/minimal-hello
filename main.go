88dc699e5d1c69317b445b3abbde494236ff4922
Alberto Marchetti <cmaster11@gmail.com>
2024-02-01 07:13:34 +0200

    Simple HTTP listener

--- main.go
+++ main.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"runtime"
 )
 
 func main() {
-	fmt.Printf("Hello world! OS: %s, arch: %s", runtime.GOOS, runtime.GOARCH)
+	argsWithoutProg := os.Args[1:]
+	command := ""
+	if len(argsWithoutProg) > 0 {
+		command = argsWithoutProg[0]
+	}
+
+	switch command {
+	case "serve":
+		serve()
+	default:
+		fmt.Printf("Hello world! OS: %s, arch: %s", runtime.GOOS, runtime.GOARCH)
+	}
+
+}
+
+func serve() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Got request!")
+		writer.Write([]byte("hello!"))
+	})
+	http.ListenAndServe(":8090", nil)
 }
